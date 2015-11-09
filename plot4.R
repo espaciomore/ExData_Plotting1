b019ac189a652267d0dbde6d12b4c7739b1c6d0b
Manuel Cerda <espaciomore@gmail.com>
2015-11-08 21:28:56 -0430

    all done

--- plot4.R
+++ plot4.R
@@ -14,9 +14,9 @@ par(mfrow = c(2,2))
 plot(hpc_df$DateTime, hpc_df$Global_active_power, type= "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 plot(hpc_df$DateTime, hpc_df$Voltage, type= "l", xlab = "", ylab = "Voltage (volts)")
 plot(hpc_df$DateTime, hpc_df$Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering")
-lines(hpc_df$DateTime, hpc_df$Sub_metering_2, col = "red")
-lines(hpc_df$DateTime, hpc_df$Sub_metering_3, col = "blue")
-sub_meterings <- colnames(hpc_df)[grepl("Sub_metering_[0-9]", colnames(hpc_df))]
-legend(x = "topright", legend = sub_meterings, lty = 1, col = c("black", "red", "blue"), cex = 0.75)
+ lines(hpc_df$DateTime, hpc_df$Sub_metering_2, col = "red")
+ lines(hpc_df$DateTime, hpc_df$Sub_metering_3, col = "blue")
+ sub_meterings <- colnames(hpc_df)[grepl("Sub_metering_[0-9]", colnames(hpc_df))]
+ legend(x = "topright", legend = sub_meterings, lty = 1, col = c("black", "red", "blue"), cex = 0.75)
 plot(hpc_df$DateTime, hpc_df$Global_reactive_power, type= "l", xlab = "", ylab = "Global Reactive Power (kilowatts)")
 dev.off()
